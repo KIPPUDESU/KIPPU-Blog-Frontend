1034c64a81746e06456055d477b849f6ce57bd9e
切符 <KIPPUDESU@OUTLOOK.com>
2025-06-07 00:40:51 +0800

    Static Structure (Upper Half)
    照着设计稿为上半部分的静态添加了基本样式

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="bg-green-400">
-        这里是头部
+    <div class="
+     relative bg-white flex h-12 w-full justify-between shadow-black/40 shadow-xs">
+        <div class="p-2 mx-6 w-46">
+            <a href="#">
+                <img src="../img/blog header.svg" alt="">
+            </a>
+        </div>
+        <div class="flex h-12 w-40 p-0.5 mx-4">
+            <div class=" text-center text-[18px] m-2 h-full w-18">注册</div>
+            <div class=" text-center text-[18px] m-2 h-full w-18">登录</div>
+        </div>
     </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div class="bg-blue-400">
-    <h2>首页内容</h2>
-    <p>欢迎来到我的网站！</p>
+  <div class="relative bg-white">
+    <div class="relative mt-8 w-full h-68 bg-gray-300/0">
+      <div class="absolute flex flex-col right-0 bottom-0 h-50 w-150 bg-green-300/0">
+        <h1 class="absolute right-20 top-4 text-[58px] font-bold">KIPPU</h1>
+        <p class="absolute right-20 top-24 text-gray-600 font-bold">这里是很短的简介，也可以很长，我不知道怎么样，就先这样</p>
+         <p class="absolute right-20 top-30 text-gray-600 font-bold">这里是很短的简介，也可以很长，我不知道怎么样，就先这样</p>
+          <p class="absolute right-20 top-36 text-gray-600 font-bold">这里是很短的简介，也可以很长，我不知道怎么样，就先这样</p>
+      </div>
+    </div>
+    <div class="relative my-4 w-full h-1 px-14">
+      <div class="bg-gray-300 h-0.5"></div>
+    </div>
+    <div class="flex flex-col">
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+      <p>内容</p>
+    </div>
   </div>
 </template>
 
