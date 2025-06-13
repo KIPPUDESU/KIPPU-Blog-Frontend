9d5409c46ec420748ea4651e416d4448de0792ef
切符 <KIPPUDESU@OUTLOOK.com>
2025-06-13 12:23:44 +0800

    Responsive Optimization for iPad Screen Sizes
    优化了昨天编写的响应式数据

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,25 +2,25 @@
   <!-- 全页面容器 -->
   <div class="relative bg-gray-300/10">
       <!-- 首页容器 -->
-    <div class="relative mt-0 w-full h-100">
+    <div class="relative w-full h-100">
       <!-- 首页图容器 -->
       <div 
-      class="relative w-full h-100 overflow-hidden">
+      class="relative h-100 overflow-hidden">
       <!-- 填充父容器的透明absolute添加图片上层添加shadow-inner -->
-      <div class="absolute z-10 inset-0 transition-all duration-600 ease-in-out" :class="ChengeStore.PlaneOrSolid?
+      <div class="absolute z-10 w-full h-full transition-all duration-600 ease-in-out" :class="ChengeStore.PlaneOrSolid?
       'shadow-[inset_0_-4px_6px_rgba(0,0,0,0.0)]' : 'shadow-[inset_0_-5px_6px_rgba(0,0,0,0.2)]'"></div>
       <img 
       class="absolute transition-all duration-600 ease-in-out
-      w-260"
+      xl:w-260 lg:w-210"
       :class="ChengeStore.PlaneOrSolid?
-      'bottom-100' : '-bottom-195'
+      'bottom-100' : 'xl:-bottom-195 lg:-bottom-150'
       "
       src="../public/img/egugang.png" alt="">
       </div>
       <!-- 顶部横线 -->
       <div 
       class="
-      transition-all duration-600 ease-in-out w-364 rounded-2xl
+      transition-all duration-600 ease-in-out xl:w-364 lg:w-278 rounded-2xl
       absolute left-1/2 -translate-x-1/2 z-10 h-0.5 bg-gray-300"
       :class="ChengeStore.PlaneOrSolid?
       'top-13' : '-top-13' "
@@ -29,13 +29,13 @@
       <div 
       class="transition-all duration-600 ease-in-out
       flex flex-col bg-gray-300/0 Noto-Sans 
-      absolute xl:mx-20 md:mx-6 z-10 font-bold text-black/60
+      absolute xl:mx-20 lg:mx-12 z-10 font-bold text-black/60
       "
-      :class="ChengeStore.PlaneOrSolid? 'xl:top-32 md:top-62' : '-top-80' ">
-      <h1 class="xl:text-[70px] md:text-[40px]">ターゲット は</h1>
+      :class="ChengeStore.PlaneOrSolid? 'xl:top-32 lg:top-38' : '-top-80' ">
+      <h1 class="xl:text-[70px] lg:text-[64px]">ターゲット は</h1>
       <!-- 文字中割线 -->
-      <div class="xl:w-120 md:w-70 xl:h-1 md:h-0.5 bg-black/50 rounded-md"></div>
-      <h1 class="xl:text-[80px] md:text-[40px]">Full Stack Developer</h1>
+      <div class="xl:w-120 lg:w-98 xl:h-1 lg:h-1 bg-black/50 rounded-md"></div>
+      <h1 class="xl:text-[80px] lg:text-[64px]">Full Stack Developer</h1>
       </div>
       <!-- 右侧文字容器 -->
       <div 
@@ -43,14 +43,14 @@
       absolute z-10 flex flex-col right-0 rounded-l-lg
       "
       :class="ChengeStore.PlaneOrSolid?
-      'bottom-0 xl:h-66 md:h-50 xl:w-140 md:w-114' : 
-      'bottom-10 xl:h-58 md:h-44 xl:w-160 md:w-120 bg-gray-500/10 backdrop-blur-xs shadow-black/20 shadow-lg'
+      'bottom-0 xl:h-66 lg:h-62 xl:w-140 lg:w-120' : 
+      'bottom-10 xl:h-58 lg:h-54 xl:w-160 lg:w-120 bg-gray-500/10 backdrop-blur-xs shadow-black/20 shadow-lg'
       "
       >
         <a href="#" @click="ChengeStore.KippuChenge">
-        <h1 class="absolute xl:right-20 md:right-6 xl:text-[92px] md:text-[70px] font-bold">KIPPU</h1>
+        <h1 class="absolute xl:right-20 lg:right-12 xl:text-[92px] lg:text-[84px] font-bold">KIPPU</h1>
         </a>
-        <div class="absolute xl:top-32 md:top-24 text-gray-600 xl:text-[16px] md:text-[14px] xl:right-20 md:right-6">
+        <div class="absolute xl:top-32 lg:top-28 text-gray-600 xl:text-[16px] lg:text-[14px] xl:right-20 lg:right-12">
         <p class=" top-32 font-bold">这里是很短的简介，也可以很长，我不知道怎么样，就先这样</p>
          <p class=" top-38 font-bold">这里是很短的简介，也可以很长，我不知道怎么样，就先这样</p>
           <p class=" top-44 font-bold">这里是很短的简介，也可以很长，我不知道怎么样，就先这样</p>
