4429adb112e7fb162e4062d5d2d1e9dfe59a0402
切符 <KIPPUDESU@OUTLOOK.com>
2025-06-23 18:02:30 +0800

    Implemented numerous CSS effects
    懒得总结了，大概就是左侧信息栏的完善吧

--- src/components/LeftInfoPanel.vue
+++ src/components/LeftInfoPanel.vue
@@ -1,13 +1,15 @@
 <template>
 <!-- 左侧信息容器 -->
 <div 
-  class="
-  absolute flex w-70 h-252 top-0 left-0
-  justify-center rounded-2xl">
+class="
+absolute w-70 h-326 top-0 left-0
+justify-center items-center">
+  <!-- 外壳 -->
+  <div class="sticky top-0 w-full h-max pb-4 flex flex-col items-center">
     <!-- 头像 -->
     <div 
     class="
-    absolute flex top-14 w-40 h-40 rounded-full justify-center items-center
+    relative mt-8 flex w-40 h-40 rounded-full justify-center items-center
     overflow-hidden shadow-lg shadow-black/30 ring-4 ring-gray-300
     ">
       <img 
@@ -20,12 +22,187 @@
     <!-- id 简介 -->
     <div 
     class="
-    absolute flex flex-col top-62 w-48 h-10 gap-1
+    relative flex flex-col w-max h-18 mt-2
     justify-center items-center
     ">
       <span class="text-2xl text-gray-800 font-bold">切符</span>
       <p class="text-md text-gray-600/80 font-extrabold">もっとElegantになりたい</p>
     </div>
+    <!-- 联系方式 -->
+    <div 
+    class="
+    relative flex flex-row w-32 h-10
+    justify-between items-center
+    ">
+      <a 
+      class="
+      transition-all duration-600 ease-in-out
+      w-8 h-8 text-blue-500/50 hover:text-blue-500/90" 
+      href="">
+        <svg 
+        version="1.1" 
+        xmlns="http://www.w3.org/2000/svg" 
+        xmlns:xlink="http://www.w3.org/1999/xlink" 
+        viewBox="0 0 512 512" 
+        enable-background="new 0 0 512 512" 
+        xml:space="preserve"
+        class="fill-current">
+          <path d="M492,109.5c-17.4,7.7-36,12.9-55.6,15.3c20-12,35.4-31,42.6-53.6c-18.7,11.1-39.4,19.2-61.5,23.5
+          C399.8,75.8,374.6,64,346.8,64c-53.5,0-96.8,43.4-96.8,96.9c0,7.6,0.8,15,2.5,22.1c-80.5-4-151.9-42.6-199.6-101.3
+          c-8.3,14.3-13.1,31-13.1,48.7c0,33.6,17.2,63.3,43.2,80.7C67,210.7,52,206.3,39,199c0,0.4,0,0.8,0,1.2c0,47,33.4,86.1,77.7,95
+          c-8.1,2.2-16.7,3.4-25.5,3.4c-6.2,0-12.3-0.6-18.2-1.8c12.3,38.5,48.1,66.5,90.5,67.3c-33.1,26-74.9,41.5-120.3,41.5
+          c-7.8,0-15.5-0.5-23.1-1.4C62.8,432,113.7,448,168.3,448C346.6,448,444,300.3,444,172.2c0-4.2-0.1-8.4-0.3-12.5
+          C462.6,146,479,129,492,109.5z">
+          </path>
+        </svg>
+      </a>
+      <a 
+      class="
+      transition-all duration-600 ease-in-out
+      w-8 h-8 text-blue-500/50 hover:text-blue-500/90" 
+      href="">
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        xmlns:xlink="http://www.w3.org/1999/xlink" 
+        viewBox="0 0 480 512"
+        class="fill-current">
+          <path d="M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9l-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9l190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284L16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z" fill="currentColor">
+          </path>
+        </svg>
+      </a>
+      <a 
+      class="
+      transition-all duration-600 ease-in-out
+      w-8 h-8 text-blue-500/50 hover:text-blue-600/90" 
+      href="">
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        xmlns:xlink="http://www.w3.org/1999/xlink" 
+        viewBox="0 0 24 24"
+        class="fill-current"
+        >
+          <path d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.09.09 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.09 16.09 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02zM8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12zm6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12z" fill="currentColor">
+          </path>
+      </svg>
+      </a>
+    </div>
+    <!-- 分类 -->
+    <div 
+    class="
+    relative flex flex-col w-49 h-92.5 mt-4
+    bg-white rounded-lg shadow-black/20 shadow-lg">
+      <!-- 图片 -->
+      <div 
+      class="
+      absolute flex z-10 inset-0 overflow-hidden rounded-lg
+      justify-center items-center bg-gradient-to-bl from-blue-300 to-pink-200">
+      <div class="absolute z-20 flex inset-0 bg-white/40 backdrop-blur-lg"></div>
+      </div>
+      <div class="flex flex-col justify-center items-center absolute inset-0 z-10">
+        <div class="
+        transition-all duration-600 ease-in-out
+        w-full object-cover hover:scale-110 bg-gradient-to-bl 
+        from-white/70 to-white/0 backdrop-blur-xs border-1 border-white
+        flex flex-row flex-1 items-center gap-2 px-4 rounded-t-lg
+        text-[16px] text-gray-600 font-medium">
+          <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          xmlns:xlink="http://www.w3.org/1999/xlink" 
+          viewBox="0 0 24 24"
+          class="w-2/16 text-gray-700">
+            <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" fill="currentColor"></path><path d="M14 17H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z" fill="currentColor">
+            </path>
+          </svg>
+          Articles Overview
+        </div>
+          <!-- 分隔 -->
+        <div class="
+        transition-all duration-600 ease-in-out
+        w-full object-cover hover:scale-110 bg-gradient-to-bl 
+        from-white/70 to-white/0 backdrop-blur-xs border-1 border-white
+        flex flex-row flex-1 items-center gap-2 px-4
+        text-[16px] text-gray-600 font-medium">
+          <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          xmlns:xlink="http://www.w3.org/1999/xlink" 
+          viewBox="0 0 32 32"
+          class="w-2/16 text-gray-700">
+            <path d="M14 4h4v4h-4z" fill="currentColor"></path><path d="M4 4h4v4H4z" fill="currentColor"></path><path d="M24 4h4v4h-4z" fill="currentColor"></path><path d="M14 14h4v4h-4z" fill="currentColor"></path><path d="M4 14h4v4H4z" fill="currentColor"></path><path d="M24 14h4v4h-4z" fill="currentColor"></path><path d="M14 24h4v4h-4z" fill="currentColor"></path><path d="M4 24h4v4H4z" fill="currentColor"></path><path d="M24 24h4v4h-4z" fill="currentColor">
+            </path>
+          </svg>
+          Categories List
+        </div>
+          <!-- 分隔 -->
+        <div class="
+        transition-all duration-600 ease-in-out
+        w-full object-cover hover:scale-110 bg-gradient-to-bl 
+        from-white/70 to-white/0 backdrop-blur-xs border-1 border-white
+        flex flex-row flex-1 items-center gap-2 px-4
+        text-[16px] text-gray-600 font-medium">
+          <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          xmlns:xlink="http://www.w3.org/1999/xlink" 
+          viewBox="0 0 24 24"
+          class="w-2/16 text-gray-700">
+            <path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H8V4h12v12zm-7.5-1a2.5 2.5 0 0 0 2.5-2.5V7h3V5h-4v5.51c-.42-.32-.93-.51-1.5-.51a2.5 2.5 0 0 0 0 5zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6z" fill="currentColor">
+            </path>
+          </svg>
+          Playlists Catalog
+        </div>
+        <!-- 分隔 -->
+        <div class="
+        transition-all duration-600 ease-in-out
+        w-full object-cover hover:scale-110 bg-gradient-to-bl 
+        from-white/70 to-white/0 backdrop-blur-xs border-1 border-white
+        flex flex-row flex-1 items-center gap-2 px-4
+        text-[16px] text-gray-600 font-medium">
+          <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          xmlns:xlink="http://www.w3.org/1999/xlink" 
+          viewBox="0 0 24 24"
+          class="w-2/16 text-gray-700">
+            <path d="M21 3H3c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5a2 2 0 0 0-2-2zm0 14H3V5h18v12zm-5-6l-7 4V7z" fill="currentColor">
+            </path>
+          </svg>
+          Video Highlights
+        </div>
+        <!-- 分隔 -->
+        <div class="
+        transition-all duration-600 ease-in-out
+        w-full object-cover hover:scale-110 bg-gradient-to-bl 
+        from-white/70 to-white/0 backdrop-blur-xs border-1 border-white
+        flex flex-row flex-1 items-center gap-2 px-4
+        text-[16px] text-gray-600 font-medium">
+          <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          xmlns:xlink="http://www.w3.org/1999/xlink" 
+          viewBox="0 0 24 24"
+          class="w-2/16 text-gray-700">
+            <path d="M12 11c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2zm6 2c0-3.31-2.69-6-6-6s-6 2.69-6 6c0 2.22 1.21 4.15 3 5.19l1-1.74c-1.19-.7-2-1.97-2-3.45c0-2.21 1.79-4 4-4s4 1.79 4 4c0 1.48-.81 2.75-2 3.45l1 1.74c1.79-1.04 3-2.97 3-5.19zM12 3C6.48 3 2 7.48 2 13c0 3.7 2.01 6.92 4.99 8.65l1-1.73C5.61 18.53 4 15.96 4 13c0-4.42 3.58-8 8-8s8 3.58 8 8c0 2.96-1.61 5.53-4 6.92l1 1.73c2.99-1.73 5-4.95 5-8.65c0-5.52-4.48-10-10-10z" fill="currentColor">
+            </path>
+          </svg>
+          Tech Insights
+        </div>
+          <!-- 分隔 -->
+        <div class="
+        transition-all duration-600 ease-in-out
+        w-full object-cover hover:scale-110 bg-gradient-to-bl 
+        from-white/70 to-white/0 backdrop-blur-xs border-1 border-white
+        flex flex-row flex-1 items-center gap-2 px-4 rounded-b-lg
+        text-[16px] text-gray-600 font-medium">
+          <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          xmlns:xlink="http://www.w3.org/1999/xlink" 
+          viewBox="0 0 24 24"
+          class="w-2/16 text-gray-700">
+            <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1s-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7-.25c.22 0 .41.1.55.25c.12.13.2.31.2.5c0 .41-.34.75-.75.75s-.75-.34-.75-.75c0-.19.08-.37.2-.5c.14-.15.33-.25.55-.25zM19 19H5V5h14v14zM12 6c-1.65 0-3 1.35-3 3s1.35 3 3 3s3-1.35 3-3s-1.35-3-3-3zm0 4c-.55 0-1-.45-1-1s.45-1 1-1s1 .45 1 1s-.45 1-1 1zm-6 6.47V18h12v-1.53c0-2.5-3.97-3.58-6-3.58s-6 1.07-6 3.58zM8.31 16c.69-.56 2.38-1.12 3.69-1.12s3.01.56 3.69 1.12H8.31z" fill="currentColor">
+            </path>
+          </svg>
+          About Me
+        </div>
+      </div>
+    </div>
+  </div>
     
 </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,7 +8,7 @@
 <!-- 这里做分隔行 -->
 
 <!-- 分页 -->
-<div class="flex relative justify-center items-center bg-gray-100 w-full h-274">
+<div class="flex relative justify-center items-center bg-gray-100 w-full h-388">
 <!-- 左侧信息容器 -->
 <LeftInfoPanel />
 <!-- 文章预览卡片 -->
@@ -16,14 +16,11 @@
   <!-- 左侧分页标题 -->
   <div 
   class="
-  absolute flex flex-col top-8 left-72 w-4 h-254
-  justify-between items-center py-6 ">
-    <div class="w-[3px] h-10 z-20 mb-2 bg-black/20 rounded-2xl"></div>
-    <span class="text-black/20 text-[20px]">記</span>
-    <span class="text-black/20 text-[20px]">事</span>
-    <span class="text-black/20 text-[20px]">一</span>
-    <span class="text-black/20 text-[20px]">覧</span>
-    <div class="w-[3px] h-210 z-20 mt-4 bg-black/20 rounded-2xl"></div>
+  absolute flex flex-col top-8 left-72 w-4 h-324
+  justify-between items-center py-6">
+      <div class="w-[3px] h-1/18 z-20 mb-2 bg-black/20 rounded-2xl"></div>
+        <span class="flex flex-col text-black/20 text-[20px]">記事一覧</span>
+      <div class="w-[3px] h-full z-20 mt-4 bg-black/20 rounded-2xl"></div>
     <div class="w-2 h-2 mt-4 bg-black/20 rounded-full"></div>
   </div>
   <!-- 边角 -->
@@ -45,20 +42,20 @@
     "></div>
     <div
     class="
-    absolute w-[3px] h-238 bg-gray-300 rounded-xl
+    absolute w-[3px] h-308 bg-gray-300 rounded-xl
     right-5 top-12
     "></div>
     <div 
     class="
     w-2 h-2 absolute bg-black/20 rounded-full
-    right-4.5 top-254
+    right-4.5 top-324.5
     "></div>
   </div>
   <!-- 三层背景容器 -->
   <div 
    class="
    transition-all duration-600 ease-in-out
-   flex flex-row absolute z-10 top-10 left-80 w-288 h-256
+   flex flex-row absolute z-10 top-10 left-80 w-288 h-316
    justify-center items-center shadow-lg
    bg-gradient-to-bl from-pink-200/50 to-pink-100/50 rounded-t-xl"
    :class="ChengeStore.PlaneOrSolid?
@@ -71,113 +68,113 @@
     justify-center items-center shadow-lg backdrop-blur-sm
     bg-gradient-to-bl from-gray-50/60 to-white/95 rounded-t-xl"
     :class="ChengeStore.PlaneOrSolid?
-    'w-288 top-0 h-256 shadow-black/0' :
-    'w-284 top-4 h-248 shadow-black/20' "
+    'w-288 top-0 h-316 shadow-black/0' :
+    'w-284 top-4 h-308 shadow-black/20' "
     >
       <div 
+      class="
+      transition-all duration-600 ease-in-out
+      flex flex-wrap absolute z-20 left-0
+      justify-center items-center shadow-lg backdrop-blur-sm
+      bg-gradient-to-bl from-gray-50/60 to-white/95 rounded-t-xl"
+      :class="ChengeStore.PlaneOrSolid?
+      'w-288 top-0 h-316 shadow-black/0' :
+      'w-280 top-4 h-300 shadow-black/20' "
+      >
+      <ArticleCard
+      v-for="article in articles"
+      :key="article.id"
+      :card="{
+        date: article.date,
+        image: article.image || '/img/egudown.png',
+        title: article.title,
+        classify: article.classify || '未分类', 
+      }"
+      :PlaneOrSolid="ChengeStore.PlaneOrSolid"
+      :to="`/`"
+      />
+      <!-- 分页按钮 -->
+      <div 
+      class="relative flex justify-center w-full h-46">
+        <div 
         class="
         transition-all duration-600 ease-in-out
-        flex flex-wrap absolute z-20 left-0
-        justify-center items-center shadow-lg backdrop-blur-sm
-        bg-gradient-to-bl from-gray-50/60 to-white/95 rounded-t-xl"
-        :class="ChengeStore.PlaneOrSolid?
-        'w-288 top-0 h-256 shadow-black/0' : 'w-280 top-4 h-240 shadow-black/20' "
+        absolute -top-2 px-18 hover:px-16
+        flex justify-between items-center w-full h-42"
         >
-        <ArticleCard
-        v-for="article in articles"
-        :key="article.id"
-        :card="{
-          date: article.date,
-          image: article.image || '/img/egudown.png',
-          title: article.title,
-          classify: article.classify || '未分类', 
-        }"
-        :PlaneOrSolid="ChengeStore.PlaneOrSolid"
-        :to="`/`"
-        />
+        <!-- 左 -->
+          <div 
+          class="flex py-2 px-4 w-56 h-12
+          hover:py-2 hover:w-54 hover:h-13
+          text-[18px] text-gray-500
+          hover:text-[20px] hover:text-gray-600
+          bg-gradient-to-br from-pink-300/60 to-white
+          hover:from-pink-300/70 hover:to-white
+          transition-all duration-500 ease-in-out
+          justify-center items-center font-bold"
+          :class="ChengeStore.PlaneOrSolid?
+          'shadow-black/20 shadow-none hover:shadow-black/20 hover:shadow-md rounded-none' : 
+          'shadow-black/20 shadow-md hover:shadow-black/30 hover:shadow-lg rounded-lg'"
+          >
+          <!-- :to="`/?page=${pageNum - 1}`" -->
+          <NuxtLink to="/" class="flex flex-nowrap items-center gap-3"
+          >
+            <!-- ← ← ← -->
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                <g transform="scale(-1,1) translate(-16,0)">
+                  <polyline 
+                    points="4,2 12,8 4,14" 
+                    fill="none" 
+                    stroke="gray" 
+                    stroke-width="2" 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round"/>
+                </g>
+              </svg>
+            Older Posts</NuxtLink>
+          </div>
+          <!-- 占位分隔图片 -->
+          <div class="flex justify-center w-max h-40">
+            <img
+            class="h-40"
+            src="../public/img/egutenshi.png" alt="">
+          </div>
+          <!-- 右 -->
+          <div 
+          class="flex py-2 px-4 w-56 h-12
+          hover:py-2 hover:w-54 hover:h-13
+          text-[18px] text-gray-500
+          hover:text-[20px] hover:text-gray-600
+          bg-gradient-to-br from-pink-300/60 to-white
+          hover:from-pink-300/70 hover:to-white
+          transition-all duration-500 ease-in-out
+          justify-center items-center font-bold"
+          :class="ChengeStore.PlaneOrSolid?
+          'shadow-black/20 shadow-none hover:shadow-black/20 hover:shadow-md rounded-none' : 
+          'shadow-black/20 shadow-md hover:shadow-black/30 hover:shadow-lg rounded-lg'"
+          >
+          <!-- :to="`/?page=${pageNum + 1}`" -->
+          <NuxtLink to="/" class="flex flex-nowrap items-center gap-3"
+          >Newer Posts 
+            <!-- → → → -->
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                  <polyline 
+                    points="4,2 12,8 4,14" 
+                    fill="none" 
+                    stroke="gray" 
+                    stroke-width="2" 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round"/>
+              </svg>
+          </NuxtLink>
+          </div>
         </div>
-    </div>
-   </div>
-   
-
-</div>
-</div>
-<!-- 分页按钮 -->
-<div 
-class="flex h-46 items-center bg-gray-100 ">
-  <div 
-  class="
-  transition-all duration-600 ease-in-out
-  px-10 hover:px-6
-  flex justify-between items-center w-288 h-46 ml-80"
-  >
-  <!-- 左 -->
-    <div 
-    class="flex py-2 px-4 w-66 h-12
-    hover:py-2 hover:w-64 hover:h-13
-    text-[20px] text-gray-500
-    hover:text-[22px] hover:text-gray-600
-    bg-gradient-to-br from-pink-300/60 to-white
-    hover:from-pink-300/70 hover:to-white
-    transition-all duration-500 ease-in-out
-    justify-center items-center font-bold"
-    :class="ChengeStore.PlaneOrSolid?
-    'shadow-black/20 shadow-none hover:shadow-black/20 hover:shadow-md rounded-none' : 
-    'shadow-black/20 shadow-md hover:shadow-black/30 hover:shadow-lg rounded-lg'"
-    >
-    <!-- :to="`/?page=${pageNum - 1}`" -->
-    <NuxtLink to="/" class="flex flex-nowrap items-center gap-3"
-    >
-      <!-- ← ← ← -->
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-          <g transform="scale(-1,1) translate(-16,0)">
-            <polyline 
-              points="4,2 12,8 4,14" 
-              fill="none" 
-              stroke="gray" 
-              stroke-width="2" 
-              stroke-linecap="round" 
-              stroke-linejoin="round"/>
-          </g>
-        </svg>
-      Older Posts</NuxtLink>
-    </div>
-    <!-- 占位分隔图片 -->
-    <div class="flex justify-center w-200 h-40 bg-red-400/0">
-      <img
-      class="h-40"
-      src="../public/img/egutenshi.png" alt="">
-    </div>
-    <!-- 右 -->
-    <div 
-    class="flex py-2 px-4 w-66 h-12
-    hover:py-2 hover:w-64 hover:h-13
-    text-[20px] text-gray-500
-    hover:text-[22px] hover:text-gray-600
-    bg-gradient-to-br from-pink-300/60 to-white
-    hover:from-pink-300/70 hover:to-white
-    transition-all duration-500 ease-in-out
-    justify-center items-center font-bold"
-    :class="ChengeStore.PlaneOrSolid?
-    'shadow-black/20 shadow-none hover:shadow-black/20 hover:shadow-md rounded-none' : 
-    'shadow-black/20 shadow-md hover:shadow-black/30 hover:shadow-lg rounded-lg'"
-    >
-    <!-- :to="`/?page=${pageNum + 1}`" -->
-    <NuxtLink to="/" class="flex flex-nowrap items-center gap-3"
-    >Newer Posts 
-      <!-- → → → -->
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-            <polyline 
-              points="4,2 12,8 4,14" 
-              fill="none" 
-              stroke="gray" 
-              stroke-width="2" 
-              stroke-linecap="round" 
-              stroke-linejoin="round"/>
-        </svg>
-    </NuxtLink>
+      </div>
+      </div>
     </div>
   </div>
+</div>
+
 </div>
 </template>
 
