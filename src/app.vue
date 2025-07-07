21222e489b4c8e1e6afb78ba06a0e8fcefe90bb6
切符 <KIPPUDESU@OUTLOOK.com>
2025-07-08 03:20:35 +0800

    Add custom transition animation
    增加了喜欢的样式

--- src/app.vue
+++ src/app.vue
@@ -2,11 +2,31 @@
 useSeoMeta({
   title: 'KIPPU-Blog:（＞ ＜）こちらは切符です'
 })
+
+import { ref } from 'vue'
+
+const nuxtApp = useNuxtApp()
+const isLoading = ref(false)
+
+nuxtApp.hook('page:start', () => {
+  isLoading.value = true;
+})
+
+nuxtApp.hook('page:finish', () => {
+  // 延迟一点时间关闭，确保动画至少播放一小段时间
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 900); 
+})
 </script>
 
 
 <template>
   <div class="">
+    <!-- 使用 Vue 的 Transition 组件来包裹遮罩层，以实现平滑的淡入淡出 -->
+    <Transition name="fade">
+      <TransitionCover v-if="isLoading" />
+    </Transition>
     <NuxtLayout>
      <NuxtPage />
     </NuxtLayout>
@@ -15,6 +35,17 @@ useSeoMeta({
 
 <style>
 @import "tailwindcss";
+
+/* 为 TransitionCover 的淡入淡出效果定义 CSS */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 
 .page-enter-active,
 .page-leave-active {
---
+++ src/components/TransitionCover.vue
@@ -0,0 +1,10 @@
+<template>
+  <div 
+  class="transition-cover absolute
+  w-full h-screen bg-gray-100 z-50">
+    <!-- 在这里放置您自定义的动画元素 -->
+    <div class="flex w-full h-full justify-center items-center">
+      <img src="../public/img/Kippusvg.svg" alt="">
+    </div>
+  </div>
+</template>
