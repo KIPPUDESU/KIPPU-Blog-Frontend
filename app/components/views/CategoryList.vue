646396c914829a0049e2616bce2b00ca54c3c955
切符 <KIPPUDESU@OUTLOOK.com>
2025-07-25 01:34:50 +0800

    feat: Enhance CategoryList with dynamic data and images
    为分类列表组件添加了动态数据和图片，使其能够展示分类名称和对应的图片

--- app/components/views/CategoryList.vue
+++ app/components/views/CategoryList.vue
@@ -2,23 +2,29 @@
   <div class="w-38/40 h-58/60">
     <div class="flex flex-wrap justify-center w-full aspect-square">
       <div
-      v-for="{item, idx} in classify"
-      class="w-80 h-80 m-4 rounded-lg bg-pink-400"></div>
+      v-for="(item, idx) in classify"
+      :key="idx"
+      class="flex relative justify-center items-center w-80 h-80 m-4 
+      rounded-lg bg-pink-400 
+      overflow-hidden">
+        <span class="absolute">{{ item.name }}</span>
+        <img class="object-cover w-full h-full" :src="item.src" alt="">
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const classify = ref([
-  { name: '', src: ''},
-  { name: '', src: ''},
-  { name: '', src: ''},
-  { name: '', src: ''},
-  { name: '', src: ''},
-  { name: '', src: ''},
-  { name: '', src: ''},
-  { name: '', src: ''},
-  { name: '', src: ''},
+  { name: '技术', src: 'classIMG/1.jpg'},
+  { name: '笔记', src: 'classIMG/2.png'},
+  { name: '动画', src: 'classIMG/3.png'},
+  { name: '随笔', src: 'classIMG/4.png'},
+  { name: '分享', src: 'classIMG/5.jpg'},
+  { name: '项目', src: 'classIMG/6.jpg'},
+  { name: '面试', src: 'classIMG/7.jpg'},
+  { name: '见闻', src: 'classIMG/8.png'},
+  { name: '杂文', src: 'classIMG/9.jpg'},
 ])
 </script>
 
