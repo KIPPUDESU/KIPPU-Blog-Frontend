b213f86d9f5a0ac60776b2e866625a475641cfe3
切符 <KIPPUDESU@OUTLOOK.com>
2025-07-24 23:24:03 +0800

    feat: Implement dynamic rendering for category list
    动态渲染分类列表，为后续添加分类内容做准备

--- app/components/views/CategoryList.vue
+++ app/components/views/CategoryList.vue
@@ -2,7 +2,7 @@
   <div class="w-38/40 h-58/60">
     <div class="flex flex-wrap justify-center w-full aspect-square">
       <div
-
+      v-for="{item, idx} in classify"
       class="w-80 h-80 m-4 rounded-lg bg-pink-400"></div>
     </div>
   </div>
@@ -10,7 +10,15 @@
 
 <script setup lang="ts">
 const classify = ref([
-  { name: '', src: ''}
+  { name: '', src: ''},
+  { name: '', src: ''},
+  { name: '', src: ''},
+  { name: '', src: ''},
+  { name: '', src: ''},
+  { name: '', src: ''},
+  { name: '', src: ''},
+  { name: '', src: ''},
+  { name: '', src: ''},
 ])
 </script>
 
